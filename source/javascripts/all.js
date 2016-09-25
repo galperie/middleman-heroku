22a5bf609eb18d5cc49423e0c76842346603b2a9
Ellen Galperin <egalperi@EGALPERIN.local>
2016-09-25 19:25:01 -0400

    Ellen - adding jquery

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,30 @@
-//= require_tree .
+$(document).ready(function() {
+
+    $("#redcircle").click(function () {
+
+        // Set the effect type
+        var effect = 'slide';
+
+        // Set the options for the effect type chosen
+        var options = { direction: 'left' };
+
+        // Set the duration (default: 400 milliseconds)
+        var duration = 700;
+
+        $('#contact').toggle(effect, options, duration);
+    });
+
+    $("#yellowcircle").click(function () {
+
+        // Set the effect type
+        var effect = 'slide';
+
+        // Set the options for the effect type chosen
+        var options = { direction: 'left' };
+
+        // Set the duration (default: 400 milliseconds)
+        var duration = 700;
+
+        $('#about').toggle(effect, options, duration);
+    });
+});
